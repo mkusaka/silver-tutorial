77f12c591684b55ff7dda93c3e9026b7e8ee19c3
mkusaka <hinoshita1992@gmail.com>
2019-03-20 00:16:55 +0900

    jokes list

--- main.go
+++ main.go
@@ -2,23 +2,45 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
+type Joke struct {
+	ID    int    `json:"id" binding:"required"`
+	Likes int    `json:"like"`
+	Joke  string `json:"joke" binding:"required"`
+}
+
+var jokes = []Joke{
+	Joke{1, 0, "Joke1"},
+	Joke{2, 0, "Joke2"},
+	Joke{3, 0, "Joke3"},
+	Joke{4, 0, "Joke4"},
+	Joke{5, 0, "Joke5"},
+	Joke{6, 0, "Joke6"},
+	Joke{7, 0, "Joke7"},
+}
+
 func JokeHandler(c *gin.Context) {
 	c.Header("Content-Type", "application/json")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Jokes handler not implemented yet",
-	})
+	c.JSON(http.StatusOK, jokes)
 }
 
 func LikeJoke(c *gin.Context) {
-	c.Header("Content-Type", "application/json")
-	c.JSON(http.StatusOK, gin.H{
-		"message": "LikeJoke handler not implemented yet",
-	})
+	if jokeid, err := strconv.Atoi(c.Param("jokeID")); err == nil {
+		for i := 0; i < len(jokes); i++ {
+			if jokes[i].ID == jokeid {
+				jokes[i].Likes += 1
+			}
+		}
+
+		c.JSON(http.StatusOK, &jokes)
+	} else {
+		c.AbortWithStatus(http.StatusNotFound)
+	}
 }
 
 func main() {
