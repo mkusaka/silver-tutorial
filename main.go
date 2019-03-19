0f302b5e35ba6e3543bfbdcc6906ebe991b07600
mkusaka <hinoshita1992@gmail.com>
2019-03-20 00:08:23 +0900

    add jokes api

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func JokeHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Jokes handler not implemented yet",
+	})
+}
+
+func LikeJoke(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "LikeJoke handler not implemented yet",
+	})
+}
+
 func main() {
 	router := gin.Default()
 
@@ -20,6 +34,9 @@ func main() {
 			})
 		})
 	}
+
+	api.GET("/jokes", JokeHandler)
+	api.POST("/jokes/like/:jokeID", LikeJoke)
 
 	router.Run(":3000")
 }
